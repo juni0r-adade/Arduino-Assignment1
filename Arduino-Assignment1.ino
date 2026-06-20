b40f6c10f631ef56fa12b24755f76ae91d0cb735
Junior Adade <junioradademartin10@gmail.com>
2026-06-20 20:58:21 +0000

    Add final Smart Countdown sketch

---
+++ Arduino-Assignment1.ino
@@ -0,0 +1,47 @@
+int startValue = 5;
+int ledPin = 13;
+
+void flashLED(int times)
+{
+    int flashes = times;
+
+    while(flashes > 0)
+    {
+        digitalWrite(ledPin, HIGH);
+        delay(200);
+
+        digitalWrite(ledPin, LOW);
+        delay(200);
+
+        flashes--;
+    }
+}
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
+
+    Serial.println("=== Smart Countdown Starting ===");
+
+    int count = startValue;
+
+    while(count > 0)
+    {
+        Serial.print("Count: ");
+        Serial.println(count);
+
+        flashLED(count);
+
+        delay(1000);
+
+        count--;
+    }
+
+    Serial.println("=== Countdown Complete ===");
+}
+
+void loop()
+{
+}
+
